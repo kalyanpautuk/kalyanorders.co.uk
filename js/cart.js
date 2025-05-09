780dc362d7945447ee26d51b6b2457ff3a40b809
kalyanpautuk <kalyanpautuk@gmail.com>
2025-05-08 15:11:02 -1000

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,8 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+  let cartCount = 0;
+  window.addToCart = function (itemName) {
+    cartCount++;
+    document.getElementById("cart-count").textContent = cartCount;
+    alert(`Order for "${itemName}" submitted. You will receive updates at +44 7450480241.`);
+  };
+});
